f856baa4243d26aeb114987e671cf01e59d8b9c2
ampatspell <ampatspell@gmail.com>
2024-04-16 21:46:26 +0300

    random

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,16 @@ import Application from './app';
 const instance = initializeApp();
 const app = new Application({ instance, logger });
 
-export const storageOnFinalized = functions.storage.onObjectFinalized({}, async (event) => {
-  app.logger.info('storage');
+export const storageOnFinalized = functions.storage.onObjectFinalized({ memory: '2GiB' }, async (event) => {
+  const contentType = event.data.contentType;
+  if(!contentType?.startsWith('image/')) {
+    return;
+  }
+
+  const name = event.data.name;
+  if(name.includes('/thumbnails/')) {
+    return;
+  }
+
+  app.logger.info('storageOnFinalized', name);
 });
