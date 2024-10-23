58b16ce05f0d2a8ad57ba08649378325219fd3f1
ampatspell <ampatspell@gmail.com>
2024-10-23 18:21:19 +0300

    Update image.svelte.ts

--- client/src/base/lib/galleries/image.svelte.ts
+++ client/src/base/lib/galleries/image.svelte.ts
@@ -15,13 +15,17 @@ export class GalleryImageRuntimeModel extends Model<GalleryImageRuntimeModelOpti
     const image = this.image;
     const runtime = this.gallery.runtime;
     const isSelected = this.isSelected;
-    if (replace) {
-      runtime.clear();
-    }
-    if (isSelected) {
-      runtime.deselect(image);
+    if(replace) {
+      if(!isSelected) {
+        runtime.clear();
+        runtime.select(image);
+      }
     } else {
-      runtime.select(image);
+      if(isSelected) {
+        runtime.deselect(image);
+      } else {
+        runtime.select(image);
+      }
     }
   }
 }
