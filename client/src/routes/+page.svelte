b6fe23d162e04e26bd4a375d425186bb96f5c67f
ampatspell <ampatspell@gmail.com>
2024-10-25 19:06:29 +0300

    Update +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,7 +2,6 @@
   import { subscribe } from '$base/lib/firebase/fire/subscriber.svelte';
   import { buildPageByIdModel } from '$base/lib/pages/page.svelte';
   import Page from '$base/components/frontend/page.svelte';
-  import { setGlobal } from '$base/lib/utils/set-global';
 
   let id = $state('NjyKMeH9VKjRaHJGy4UG');
   let page = $derived(buildPageByIdModel({ id }));
