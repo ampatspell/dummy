69d5ae7e0c83fd0ac3425f72d0a75315011c20d1
Arnis Vuskans <ampatspell@gmail.com>
2024-11-09 14:28:14 +0200

    dummy layout settings

--- client/src/lib/layouts/dummy/backend.svelte
+++ client/src/lib/layouts/dummy/backend.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import Header from '$dummy/components/dark/inspector/header.svelte';
+  import InputRow from '$dummy/components/dark/inspector/input-row.svelte';
   import Inspector from '$dummy/components/dark/inspector/inspector.svelte';
   import Section from '$dummy/components/dark/inspector/section.svelte';
-  import ValueRow from '$dummy/components/dark/inspector/value-row.svelte';
   import type { LayoutModel } from '$dummy/lib/layouts/layout.svelte';
   import type { DummyLayoutSettingsModel } from './settings.svelte';
 
   let { layout }: { layout: LayoutModel } = $props();
   let settings = $derived(layout.settings as DummyLayoutSettingsModel);
+
+  let properties = $derived(settings.properties);
+  let title = $derived(properties.title);
 </script>
 
 <Inspector>
@@ -15,7 +18,6 @@
     <Header title="'Dummy' layout" />
   </Section>
   <Section>
-    <ValueRow label="Layout" value={layout.description} />
-    <ValueRow label="Settings" value={settings.description} />
+    <InputRow label="Title" property={title} />
   </Section>
 </Inspector>
--- client/src/lib/layouts/dummy/settings.svelte.ts
+++ client/src/lib/layouts/dummy/settings.svelte.ts
@@ -1,4 +1,20 @@
 import { LayoutSettingsModel } from '$dummy/lib/layouts/layout.svelte';
+import { getter } from '$dummy/lib/utils/options';
+import { Properties, Property, type PropertiesOptions } from '$dummy/lib/utils/property.svelte';
+
+export type DummyLayoutSettingsPropertiesModelOptions = {
+  settings: DummyLayoutSettingsModel;
+} & PropertiesOptions;
+
+export class DummyLayoutSettingsPropertiesModel extends Properties<DummyLayoutSettingsPropertiesModelOptions> {
+  data = $derived(this.options.settings.data);
+
+  title = new Property<string>({
+    delegate: this,
+    value: getter(() => this.data.title),
+    update: (value) => (this.data.title = value),
+  });
+}
 
 export type DummyLayoutSettings = {
   title: string;
@@ -6,4 +22,9 @@ export type DummyLayoutSettings = {
 
 export class DummyLayoutSettingsModel extends LayoutSettingsModel<DummyLayoutSettings> {
   isLoaded = true;
+
+  properties = new DummyLayoutSettingsPropertiesModel({
+    settings: this,
+    didUpdate: () => this.save(),
+  });
 }
