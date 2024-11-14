805396aa4412e2eacfacfc8cd34d9c3d9355543e
Arnis Vuskans <ampatspell@gmail.com>
2024-11-14 19:25:51 +0200

    Update backend.svelte

--- client/src/dummy/components/frontend/backend.svelte
+++ client/src/dummy/components/frontend/backend.svelte
@@ -10,7 +10,7 @@
 </script>
 
 {#if isAdmin}
-  <a href={route} class="backend">
+  <a href={route} target="_blank" class="backend">
     <Icon size="small" icon={LucideSquareCode} />
   </a>
 {/if}
